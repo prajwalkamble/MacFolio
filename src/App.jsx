56a92cde392fbc694a7dca297e9e421b9ddddd76
Prajwal <prajwalkamble342000@gmail.com>
2025-11-23 01:23:24 +0530

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Navbar from "#components/Navbar.jsx";
+import {Navbar, Welcome } from "#components";
 
 const App = () => {
-    return (
-        <main>
-            <Navbar />
-        </main>
-    )
+  return (
+    <main>
+      <Navbar />
+      <Welcome />
+    </main>
+  )
 }
+
 export default App
